f650beb97b5c6128222a31591995e92318571859
Liudmila Gonchikova <gonchikova94@gmail.com>
2020-09-18 04:07:35 +0800

    initial commit

---
+++ src/main/java/com/gly/TestTask4/TestTask4Application.java
@@ -0,0 +1,13 @@
+package com.gly.TestTask4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTask4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTask4Application.class, args);
+	}
+
+}
